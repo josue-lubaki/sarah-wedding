ccda2660b790b0ac751e2cc08bf28ac93c98e34d
Josue Lubaki <josuelubaki@gmail.com>
2026-02-07 02:52:50 -0500

    ✨ Update output directory from 'dist' to 'docs' in Vercel and Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   base: mode === "production" ? "/sarah-wedding/" : "/",
   build: {
-    outDir: "dist",
+    outDir: "docs",
   },
   server: {
     host: "::",
